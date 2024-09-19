b422e24e155c38195cdac6f8d072ca8fb4443d54
andejuli <techtrek@hotmail.com>
2024-09-19 13:28:11 -0600

    updated css

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+let disclaimer = document.querySelector(".disclaimer a");
+
+disclaimer.innerHTML = parkData.fullName;
+disclaimer.href = parkData.url;
+
+document.querySelector('title').innerHTML = parkData.fullName;
+
+let banner_img = document.querySelector(".hero-banner img");
+
+banner_img.src = parkData.images[0].url;
+banner_img.alt = parkData.images[0].altText;
+
+document.querySelector('.hero-banner_title').innerHTML = parkData.name;
+document.querySelector('#park_des').innerHTML = parkData.designation;
+document.querySelector('#states').innerHTMl = parkData.states;
+
+
+
+
+
+
+
+  
