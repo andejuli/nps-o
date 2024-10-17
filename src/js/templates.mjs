2e94d5e773529b526d74ef770fd0ecafc18e1351
andejuli <techtrek@hotmail.com>
2024-10-17 11:02:12 -0600

    take out /

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -40,7 +40,7 @@ export function alertTemplate(data) {
     console.log(data.title);
     console.log(data.description);
     return `<li class="alert">
-  <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use></svg>
+  <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="images/sprite.symbol.svg#alert-${alertType}"></use></svg>
   <div>  
     <h3 class="alert-${alertType}">${data.title}</h3>
     <p>${data.description}</p>
