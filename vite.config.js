6e09a702344a7f556aa476138930507486aff490
andejuli <techtrek@hotmail.com>
2024-12-02 13:24:55 -0700

    change

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         conditions: resolve(__dirname, "src/conditions.html"),
-        visitorCenter: resolve(__dirname, "src/visitor-center.html")
+        visitor-center: resolve(__dirname, "src/visitor-center.html")
         
       }
     }
