aed065645e57eda8e0a93f40ed28b66dec5a7143
andejuli <techtrek@hotmail.com>
2024-12-02 13:04:21 -0700

    add page again

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         conditions: resolve(__dirname, "src/conditions.html"),
-        conditions: resolve(__dirname, "src/visitor-center.html"),
+        conditions: resolve(__dirname, "src/visitor-center.html")
       }
     }
   }
